c8e49eb7a6fddb38b64e5c17cc18848fc978a1ac
mazpeng <mazgpeng@gmail.com>
2023-12-29 20:56:12 +0700

    function registrasi

--- function.php
+++ function.php
@@ -149,3 +149,40 @@ function cari($keyword)
 
     return query($query);
 }
+
+function registrasi($data)
+{
+    global $conn;
+
+    $username = strtolower(stripslashes($data["username"]));
+    $password = mysqli_real_escape_string($conn, $data["password"]);
+    $password2 = mysqli_real_escape_string($conn, $data["password2"]);
+
+    //cek username duplikat
+    $result = mysqli_query($conn, "SELECT username FROM user WHERE username = '$username'");
+
+    if (mysqli_fetch_assoc($result)) {
+        echo "<script>
+                alert('username already exists');
+                </script>";
+        return false;
+    }
+
+    //konfirmasi password
+    {
+        if ($password !== $password2) {
+            echo "<script>
+                    alert('password incorrect');
+                </script>";
+            return false;
+        }
+    }
+
+    //enkripsi password
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    //tambah user baru ke database
+    mysqli_query($conn, "INSERT INTO user VALUES('', '$username', '$password')");
+
+    return mysqli_affected_rows($conn);
+}
